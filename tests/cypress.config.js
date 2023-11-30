bf41221d599df4943323abf8b2db93338c255419
TRZMlEL <mateusztrzmiel2104@gmail.com>
2023-11-30 14:47:14 +0100

    cypress config

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
-  component: {
-    devServer: {
-      framework: 'vue',
-      bundler: 'vite',
-    },
-  },
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
